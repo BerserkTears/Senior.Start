a39062ab1c64ca04473f417f9f042ff3b26bae51
BerserkTears <kirill.kunitsyn@gmail.com>
2021-05-05 01:07:21 +0900

    index 0.1.0
    Создан index.php с ссылками на вход в аккаунт и регистрацию обычных пользователей и компаний
    Начата работа над регистрацией пользователей

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Senior.Start</title>
+</head>
+<body>
+    <a href="login.php">Вход</a>
+    <a href="userregistrater.php">Регистрация</a>
+    <a href="cregistration.php">Регистрация компании</a>
+</body>
+</html>
---
+++ userregister.php
@@ -0,0 +1,39 @@
+<?php
+require "db.php";
+
+$data=$_POST;
+if (isset($data['do_reg'])){
+    if (R::count('users', "login = ?", array($data['login'])) > 0) {echo '<script>alert("Пользователь зарегистрирован")</script>';} else {
+        if($data['password']==$data['password1']){
+            $user=R::dispense('users');
+            $user -> login = $data['login'];
+            $user -> password = $data['password'];
+            $user -> name = $data['name'];
+            $user -> surname = $data['surname'];
+            $user -> company = "none";
+            R::store($user);
+            $_SESSION['logged_user'] = $user;
+            header('Location: /lk.php');
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Регистрация</title>
+</head>
+<body>
+    <form action="/registration.php" method="POST"> 
+        <input type="text" name="name" required placeholder = "Имя">
+        <input type="text" name="surname" required placeholder = "Фамилия">
+        <input type="email" name="login" value= "<?php echo @$data['login'] ?>" required placeholder = "Логин(почта)"> 
+        <input type="password" name="password" required placeholder = "Пароль">
+        <input type="password" name="password1" required placeholder = "Подтвердите пароль">
+        <button type="submit" name = "do_reg"> Зарегистрироваться </button>
+    </form>
+</body>
+</html>
